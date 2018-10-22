c7f0d2c8eb9953a2d09aa90cdbe4c1dc30507ac8
Sushil Laxman Shinde <sushil@brandmovers.in>
2018-10-22 18:40:53 +0530

    laravel authentication added, api routes added

--- Source/routes/api.php
+++ Source/routes/api.php
@@ -16,3 +16,32 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+//Route::group([
+//    'prefix' => 'auth', 'middleware:auth'
+//], function () {
+//    Route::post('login', 'AuthController@login');
+//    Route::post('signup', 'AuthController@signup');
+//
+//    Route::group([
+//        'middleware' => 'auth:api'
+//    ], function() {
+//        Route::get('logout', 'AuthController@logout');
+//        Route::get('user', 'AuthController@user');
+//    });
+//});
+
+Route::group(['prefix' => 'auth'], function() {
+    Route::post('register', 'AuthController@register');
+
+
+    Route::group(['middleware' => 'auth:api'], function() {
+        Route::get('user', function (Request $request){
+           return $request->user();
+        });
+
+        Route::get('logout', 'AuthController@logout');
+
+    });
+});
